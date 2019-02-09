7d5dd594a538ec2012bae6f916aa7b28fbde87c8
Christopher Henn <chris@chrishenn.net>
2019-02-09 13:41:48 -0800

    Tweak slideshow

--- static/slideshow.js
+++ static/slideshow.js
@@ -1,7 +1,6 @@
 setInterval(() => {
   const slides = Array.from(document.querySelectorAll('.slideshow li'))
-  const activeIndex = slides.findIndex(slide => slide.classList.contains('active'))
 
-  slides[activeIndex].classList.remove('active')
-  slides[(activeIndex + 1) % slides.length].classList.add('active')
+  slides.find(slide => slide.classList.contains('active')).classList.remove('active')
+  slides[Math.floor(Math.random() * slides.length)].classList.add('active')
 }, 7000)
