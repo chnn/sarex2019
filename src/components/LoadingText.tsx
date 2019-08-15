a7234294e73388d033080e486da8f7acb68e396f
Christopher Henn <chris@chrishenn.net>
2019-08-15 09:43:23 -0700

    Close registration

--- src/components/LoadingText.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-import "./LoadingText.css"
-
-const LoadingText = ({ text = "Loading" }) => {
-  return <div className="loading-text">{text}</div>
-}
-
-export default LoadingText
